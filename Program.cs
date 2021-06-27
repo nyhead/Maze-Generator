649aa94643abae6bacd24cf84789e0d54a92025f
amirjann <amirjann@gitlab.mff.cuni.cz>
2021-06-27 21:30:40 +0400

    updated error handling

--- Program.cs
+++ Program.cs
@@ -2,37 +2,40 @@ using System;
 using static System.Console;
 
 class Program {
-    static void Main(string[] args) {          
-      Maze maze = null;
-      Solver solver = null;
-      string out_filename = "";
-      for (int i = 0 ; i < args.Length ; ++i) 
-          switch (args[i]) {
-              case "-g":
-                  int width = int.Parse(args[i+1]), height = int.Parse(args[i+2]);
-                  maze = new Maze(width, height);
-                  out_filename = args[i+3];
-                  i += 3;
-                  break;
-              case "-s":
-                  Solver.Profile choice = args[i+1] == "dfs" ? Solver.Profile.DFS : args[i+1] == "bfs" ? Solver.Profile.BFS : Solver.Profile.Unknown;
-                  maze = new Maze(args[i+2]);
-                  solver = new Solver(maze, choice);
-                  out_filename = args[i+3];
-                  i += 3;
-                  break;
-              default:
-                  WriteLine("ignored the option: " + args[i]);                  
-                  break;
-          } 
 
-      if (maze is Maze) {
-        Canvas canvas = new Canvas(maze);
-        canvas.draw_the_maze(solver);
-        canvas.save_image(out_filename);
-        return;
-      }
-      
-      WriteLine("usage: program [-g <w> <h> <output_filename>.png] [-s dfs/bfs <input_filename>.png <output_filename>.png]");          
+    static void display_user_message() => WriteLine("usage: program [-g <w> <h> <output_filename>.png] [-s dfs/bfs <input_filename>.png <output_filename>.png]");
+    static void Main(string[] args) {       
+        Maze maze = null;
+        Solver solver = null;
+        string out_filename = "";
+        for (int i = 0 ; i < args.Length ; ++i) 
+            switch (args[i]) {
+                case "-g":
+                    int width = int.Parse(args[i+1]), height = int.Parse(args[i+2]);
+                    maze = new Maze(width, height);
+                    out_filename = args[i+3];
+                    i += 3;
+                    break;
+                case "-s":
+                    Solver.Profile choice = args[i+1] == "dfs" ? Solver.Profile.DFS : args[i+1] == "bfs" ? Solver.Profile.BFS : Solver.Profile.Unknown;
+                    maze = new Maze(args[i+2]);
+                    solver = new Solver(maze, choice);
+                    out_filename = args[i+3];
+                    i += 3;
+                    break;
+                default:
+                    WriteLine("invalid option: " + args[i]);                  
+                    display_user_message();
+                    return;
+            } 
+
+        if (maze is Maze) {
+            Canvas canvas = new Canvas(maze);
+            canvas.draw_the_maze(solver);
+            canvas.save_image(out_filename);
+            return;
+        }
+        
+        display_user_message();                  
     }    
 }
